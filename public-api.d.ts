cb9154bf0725fb6457dbf6ae5a7f2a421d60a9f9
Andreas Rath <andreas.rath@ondewo.com>
2024-11-10 13:13:12 +0100

    Upgraded to proto compiler 5.1.0

---
+++ public-api.d.ts
@@ -0,0 +1,4 @@
+export * from './api/ondewo/t2s/text-to-speech_grpc_web_pb.d';
+export * from './api/ondewo/t2s/text-to-speech_pb.d';
+export * from './api/google/protobuf/empty_pb.d';
+export * from './api/google/protobuf/struct_pb.d';
